e328da454ec0e9fa09b03d9fd9daeab7c95a7008
Ivan Tabarino <tabarino@outlook.com>
2021-03-27 17:11:58 +0000

    Working with Props and Children Props

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working!</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name="Ivan" age="39">My Hobby is Develop.</Person>
+        <Person name="Rosana" age="37">My Hobby is Travel.</Person>
+        <Person name="Matheus" age="19">My Hobby is Sleep.</Person>
       </div>
     );
 
