947f10da76f4f5ea3f5ed525279e2941fc3b81d1
Ivan Tabarino <tabarino@outlook.com>
2021-03-31 10:53:57 +0100

    Setting Styles Dynamically

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -39,7 +40,9 @@ class App extends Component {
             );
           })}
         </div>
-      )
+      );
+
+      style.backgroundColor = 'red';
     }
 
     return (
