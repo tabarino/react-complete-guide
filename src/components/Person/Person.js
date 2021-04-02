580d9f6f0e9cd9d6aa5997f3f9b55643c410376a
Ivan Tabarino <tabarino@outlook.com>
2021-04-02 12:43:52 +0100

    Rendering Adjacent JSX Elements

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import Aux from '../../hoc/Aux';
 import styles from "./Person.module.css";
 
-const Person = (props) => {
+class Person extends Component {
+  // const Person = (props) => {
   // const rnd = Math.random();
   // if (rnd > 0.7) {
   //   throw new Error('Something went wrong!!!');
   // }
 
-  console.log('Person render');
+  render() {
+    console.log('Person render');
 
-  return (
-    <div className={styles.Person}>
-      <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}></input>
-    </div>
-  );
+    // You can use Adjacent JSX Element returning an array
+    // return [
+    //   <p key="item1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>,
+    //   <p key="item2">{this.props.children}</p>,
+    //   <input key="item3" type="text" onChange={this.props.changed} value={this.props.name}></input>
+    // ];
+
+    // Or in a better way, just create an Aux component and wrap everything inside it
+    // return (
+    //   <Aux>
+    //     <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+    //     <p>{this.props.children}</p>
+    //     <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+    //   </Aux>
+    // );
+
+    // From React 16 you can use like this
+    return (
+      <Fragment>
+        <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+      </Fragment>
+    );
+  }
+
+  // return (
+  //   <div className={styles.Person}>
+  //     <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</p>
+  //     <p>{props.children}</p>
+  //     <input type="text" onChange={props.changed} value={props.name}></input>
+  //   </div>
+  // );
 }
 
 export default Person;
