a333e23faebf4deb526a68fed9e6a9aa2f2bfbb4
Ivan Tabarino <tabarino@outlook.com>
2021-04-02 11:07:14 +0100

    Using useEffect() in Functional Components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
+  // It is like componentDidMount and componentDidUpdate and just this Effect
+  useEffect(() => {
+    console.log('Cockpit useEffect');
+    // You can send a HTTP Request from here
+  });
+
   const assignedClasses = [];
   let buttonClass = '';
 
