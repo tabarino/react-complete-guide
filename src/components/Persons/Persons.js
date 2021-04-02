a7a71adfa009a92ca3faeaa35d5daa98ebb5abbb
Ivan Tabarino <tabarino@outlook.com>
2021-04-02 10:38:43 +0100

    Component Update Lifecycle (for props changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from '../Person/Person';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
-const Persons = (props) => {
-  console.log('Persons render');
-  
-  return props.persons.map((person, index) => {
-    return (
-      <ErrorBoundary key={person.id}>
-        <Person
-          click={props.clicked.bind(this, index)}
-          changed={(event) => props.changed(event, person.id)}
-          name={person.name}
-          age={person.age}>
-        </Person>
-      </ErrorBoundary>
-    );
-  });
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('Persons getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Persons shouldComponentUpdate');
+    return true;
+  }
+
+  render() {
+    console.log('Persons render');
+
+    return this.props.persons.map((person, index) => {
+      return (
+        <ErrorBoundary key={person.id}>
+          <Person
+            click={this.props.clicked.bind(this, index)}
+            changed={(event) => this.props.changed(event, person.id)}
+            name={person.name}
+            age={person.age}>
+          </Person>
+        </ErrorBoundary>
+      );
+    });
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Persons getSnapshotBeforeUpdate');
+    return { message: 'Snapshot' };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Persons componentDidUpdate', snapshot);
+  }
 }
 
 export default Persons;
